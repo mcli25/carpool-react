fe74698e02212cbaeecc49a9bf19ca5885e60cab
Bruce <xuayng.wu1109@gmail.com>
2023-11-24 15:27:36 +0000

    add new folder and CarOwnerComponent

---
+++ src/components/CarOwner/CarOwnerComponent.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import PublishComponent from "./PublishComponent";
+import PopularRoutesComponent from "./PopularRoutesComponent";
+import CarInfoComponent from "./CarInfoComponent";
+
+function App() {
+
+  const [displayComponent, setDisplayComponent] = useState(null);
+
+  const showCarInfoComponent = () => {
+    setDisplayComponent(<CarInfoComponent />);
+  }
+
+  const showPopularRoutesComponent = () => {
+    setDisplayComponent(<PopularRoutesComponent />)
+  }
+
+  const showPublishComponent = () => {
+    setDisplayComponent(<PublishComponent />)
+  }
+
+  return (
+      <div>
+        <h1>Car Owner</h1>
+        <button onClick={showCarInfoComponent}>showCarInfoComponent</button> 
+        <button onClick={showPopularRoutesComponent}>showPopularRoutesComponent</button>
+        <button onClick={showPublishComponent}>showPublishComponent</button>
+        {displayComponent}
+      </div>
+  );
+}
+
+export default App;
