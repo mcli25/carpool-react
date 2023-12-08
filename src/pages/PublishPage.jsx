90ca8eaf19e564dd33f15486424a84b0d05b0fd1
CyberBruce <cyberbruce24@gmail.com>
2023-12-08 15:42:42 +0000

    [test] Pushing code

--- src/pages/PublishPage.jsx
+++ src/pages/PublishPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Publish from "../components/CarOwner/Publish";
 
 const PublishPage = () => {
-  return <Publish></Publish>;
+  return <div>Publish Test</div>;
 };
 
 export default PublishPage;
