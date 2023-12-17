ac2bc62cfb25371e437b70c17a062694c3819346
CyberBruce <cyberbruce24@gmail.com>
2023-12-17 16:24:38 +0000

    [update] Add vehicel info

--- src/components/CarOwner/CarInfo.jsx
+++ src/components/CarOwner/CarInfo.jsx
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 
 const CarInfo = () => {
   const [vehicleInfo, setVehicleInfo] = useState({
+    PlateNumber: "13-954",
     Name: "Sprinter Trueno AE86",
-    Make: "Toyota",
+    Manufacturer: "Toyota",
     Model: "AE86",
     Year: 1987,
     Color: "Write/Black",
     Type: "Compact Car",
     Layout: "Front-Engined, Rear-Wheel Drive",
+    EngineType: "4A-GE 1.6L Inline-4",
+    MaximumPower: "130 horsepower (97 kW)",
+    Transmission: "5-speed manual or 4-speed automatic",
+    TireSize: "185/60HR14",
+    Weight: "Approximately 950 kg",
     imageUrl: "/img/AE86.JPG",
   });
   return (
